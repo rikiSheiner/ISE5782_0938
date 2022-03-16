01993f3cfdc07203183d922cf57169a279db0adb
riki <r0587860843@gmail.com>
2022-03-16 13:25:17 +0200

    part3

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -6,16 +6,36 @@ import primitives.Ray;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Geometries class represents a collection of geometric shapes
+ * @author Rivka Sheiner
+ */
 public class Geometries implements Intersectable{
 
+    /**
+     * The list of the geometric shapes
+     */
     private List<Intersectable> geometries;
 
+    /**
+     * Geometries default constructor
+     */
     public Geometries() {
         geometries = new LinkedList<>();
     }
+
+    /**
+     * Geometries parameters constructor
+     * @param geometries the geometric shapes
+     */
     public Geometries(Intersectable... geometries){
         this.geometries = List.of(geometries);
     }
+
+    /**
+     * This method adds new geometric shapes to the collection
+     * @param geometries the shapes for adding
+     */
     public void add(Intersectable... geometries){
         for(int i=0; i<geometries.length; i++) {
             this.geometries.add(geometries[i]);
--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -3,6 +3,15 @@ package geometries;
 import primitives.*;
 import java.util.List;
 
+/**
+ * Intersectable interface represents the behavior of all geometry objects
+ *@author Rivka Sheiner
+ */
 public interface Intersectable {
+    /**
+     * The method finds the intersection points of the geometry with the ray
+     * @param ray - the ray imposed on the geometric shape
+     * @return List of Point - the intersection points with the ray
+     */
     public List<Point> findIntersections(Ray ray);
 }
